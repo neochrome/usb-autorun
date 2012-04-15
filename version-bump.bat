7f88131677b139f838bf79869d921ab3a43a8857
neochrome <johan@stenqvist.net>
2012-04-15 23:03:34 +0200

    version bumping fixed

--- version-bump.bat
+++ version-bump.bat
@@ -1,4 +1,10 @@
 @echo off
+setlocal
+set git=git
+for /f "delims=" %%a in ('where git') do set gitext=%%~xa
+if '%gitext%' == '.cmd' set git=call %git%
+if '%gitext%' == '.bat' set git=call %git%
+
 set version=%1
 if not '%version%'=='' goto set_version
 echo usage: %0 version
@@ -11,9 +17,9 @@ goto :fail
 :set_version
 echo [assembly: System.Reflection.AssemblyVersion("%version%")] > version.cs
 echo [assembly: System.Reflection.AssemblyFileVersion("%version%")] >> version.cs
-git add version.cs
-git commit -m "version bump to v%version%"
-git tag v%version%
+%git% add version.cs
+%git% commit -m "version bump to v%version%"
+%call% git tag v%version%
 goto success
 
 :fail
